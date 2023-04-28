474e76bd2b523436c9e418c7ec4ae48907088166
Gaston Valles <valles.gastond@hotmail.com>
2023-04-28 13:03:27 -0300

    comando start

--- api/src/routes/config.js
+++ api/src/routes/config.js
@@ -3,4 +3,4 @@ export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "chaca";
 export const DB_NAME = process.env.DB_NAME || "db_user";
-export const DB_PORT = process.env.DB_PORT || 5432;
+export const DB_PORT = process.env.DB_PORT || 3006;
